0739f1ad305b1c05f5dd330fbcc738dd678f9843
Adson Mozart <mozar7.dev@gmail.com>
2026-08-31 12:45:49 -0300

    test: refining the exceptions

--- domain/src/main/java/com/mozart/admin/catalogo/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/mozart/admin/catalogo/domain/exceptions/DomainException.java
@@ -3,17 +3,21 @@ package com.mozart.admin.catalogo.domain.exceptions;
 import java.util.List;
 import com.mozart.admin.catalogo.domain.validation.Error;
 
-public class DomainException extends RuntimeException{
+public class DomainException extends NoStacktraceException{
 
     private final List<Error> errors;
 
-    private DomainException(final List<Error> anErrors) {
-        super("", null, true, false);
+    private DomainException(final String aMassage, final List<Error> anErrors) {
+        super(aMassage);
         this.errors = anErrors;
     }
 
+    public static DomainException with(final Error anErrors) {
+        return new DomainException(anErrors.message(), List.of(anErrors));
+    }
+
     public static DomainException with(final List<Error> anErrors) {
-        return new DomainException(anErrors);
+        return new DomainException("", anErrors);
     }
 
     public List<Error> getErrors() {
